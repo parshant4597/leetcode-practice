75439d58c7645e0749ab3404358bf88b635dbc19
parshant4597 <149263314+parshant4597@users.noreply.github.com>
2026-06-10 12:33:26 +0530

    Time: 11 ms (60.24%), Space: 104.1 MB (82.79%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
@@ -1,20 +1,28 @@
 class Solution {
     int count = 0;
-        
+
     public void preorder(TreeNode node, int path) {
-        if (node != null) { 
-            path = path ^ (1 << node.val); 
-            if (node.left == null && node.right == null) { 
-                if ((path & (path - 1)) == 0) {
+        if (node != null) {
+            path = path ^ (1 << node.val);
+            if (node.left == null && node.right == null) {
+                int oddCount = 0;
+
+                for (int i = 1; i <= 9; i++) {
+                    if ((path & (1 << i)) != 0) {
+                        oddCount++;
+                    }
+                }
+
+                if (oddCount <= 1) {
                     ++count;
                 }
             }
             preorder(node.left, path);
-            preorder(node.right, path) ;
+            preorder(node.right, path);
         }
     }
 
-    public int pseudoPalindromicPaths (TreeNode root) {
+    public int pseudoPalindromicPaths(TreeNode root) {
         preorder(root, 0);
         return count;
     }
