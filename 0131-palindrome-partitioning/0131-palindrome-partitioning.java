902cd1dbff499f9fd97ffce972ca9c33288eeb36
parshant4597 <149263314+parshant4597@users.noreply.github.com>
2026-06-11 13:20:18 +0530

    Time: 8 ms (72.82%), Space: 65.1 MB (72.61%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<String>> partition(String s) { 
+        List<List<String>> res = new ArrayList<>(); 
+        List<String> path = new ArrayList<>(); 
+        dfs(0, s, path, res);
+        return res;
+    }
+
+    private void dfs(int index, String s, List<String> path, List<List<String>> res) { 
+        if (index == s.length()) { 
+            res.add(new ArrayList<>(path));
+            return;
+        } 
+        for (int i = index; i < s.length(); i++) { 
+            if (isPalindrome(s, index, i)) { 
+                path.add(s.substring(index, i + 1)); 
+                dfs(i + 1, s, path, res); 
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int start, int end) { 
+        while (start <= end) { 
+            if (s.charAt(start++) != s.charAt(end--))
+                return false;
+        }
+        return true;  
+    }
+};
