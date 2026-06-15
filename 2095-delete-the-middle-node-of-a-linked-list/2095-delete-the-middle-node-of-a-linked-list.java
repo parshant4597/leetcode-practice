76c05ff7506660050e6850fe0579725f9363daaf
parshant4597 <149263314+parshant4597@users.noreply.github.com>
2026-06-15 13:50:21 +0530

    Time: 3 ms (99.96%), Space: 202.7 MB (22.05%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -1,36 +1,52 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+// /**
+//  * Definition for singly-linked list.
+//  * public class ListNode {
+//  *     int val;
+//  *     ListNode next;
+//  *     ListNode() {}
+//  *     ListNode(int val) { this.val = val; }
+//  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//  * }
+//  */
+// class Solution {
+//     public ListNode deleteMiddle(ListNode head) {
+//         ListNode slow = head ;  
+//         ListNode fast = head ; 
+//         ListNode temp = head ; 
+//         if(head.next == null){
+//             return null ; 
+//         } 
+//         // 1 2 3 4 
+//         // temp 1 
+//         // slow 2 
+//         // fast 3 
+//         while(fast != null && fast.next != null ){
+//             temp = slow ;
+//             slow = slow.next ; 
+//             if(fast.next.next == null){
+//                 fast = null  ;
+//             }else fast = fast.next.next ; 
+             
+//         }
+//         slow  = slow.next ; 
+//         temp.next = slow ; 
+//         return head ; 
+        
+//     }
+// }
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-        ListNode slow = head ;  
-        ListNode fast = head ; 
-        ListNode temp = head ; 
-        if(head.next == null){
-            return null ; 
+        if (head.next == null) return null;
+
+        ListNode slow = head;
+        ListNode fast = slow.next.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        // 1 2 3 4 
-        // temp 1 
-        // slow 2 
-        // fast 3 
-        while(fast != null && fast.next != null ){
-            temp = slow ;
-            slow = slow.next ; 
-            if(fast.next.next == null){
-                fast = null  ;
-            }else fast = fast.next.next ; 
-             
-        }
-        slow  = slow.next ; 
-        temp.next = slow ; 
-        return head ; 
-        
+
+        slow.next = slow.next.next;
+        return head;
     }
 }
