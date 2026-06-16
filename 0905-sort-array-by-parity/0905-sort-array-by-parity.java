5927e40518d494bd3662a2161899e040cdbe7034
parshant4597 <149263314+parshant4597@users.noreply.github.com>
2026-06-16 15:50:20 +0530

    Time: 0 ms (100%), Space: 47 MB (29.71%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -1,3 +1,22 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int left = 0;
+        int right = 0;
+        while(right < nums.length && left < nums.length){
+            if(nums[right]%2 != 0){
+                right++;
+            }else{
+                int temp = nums[left];
+                nums[left]  = nums[right];
+                nums[right] =temp ; 
+                left++; 
+                right++;
+            }
+        }
+        return nums ; 
+
+    }
+}
 // class Solution {
 //     public int[] sortArrayByParity(int[] nums) {
 //         int left = 0;
@@ -20,19 +39,19 @@
 
 //     }
 // }
-class Solution {
-    public int[] sortArrayByParity(int[] nums) {
-        int left = 0;
+// class Solution {
+//     public int[] sortArrayByParity(int[] nums) {
+//         int left = 0;
+        
+//         for (int i = 0; i < nums.length; i++) {
+//             if (nums[i] % 2 == 0) {
+//                 int temp = nums[left];
+//                 nums[left] = nums[i];
+//                 nums[i] = temp;
+//                 left++;
+//             }
+//         }
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 == 0) {
-                int temp = nums[left];
-                nums[left] = nums[i];
-                nums[i] = temp;
-                left++;
-            }
-        }
-
-        return nums;        
-    }
-}
+//         return nums;        
+//     }
+// }
