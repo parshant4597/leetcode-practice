6ffaf4b379b5a64588580dc0dc9832df2ad67407
parshant4597 <149263314+parshant4597@users.noreply.github.com>
2026-06-19 14:51:26 +0530

    Time: 11 ms (7.06%), Space: 47.6 MB (56.44%) - LeetHub

---
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.java
@@ -0,0 +1,54 @@
+class Solution {
+    public long minimumSteps(String s) {
+        long ans = 0 ; 
+        //
+    
+        int n = s.length() ;
+        int[] suffix = new int[n];
+        if(s.charAt(n-1) == '1'){
+            suffix[n-1] = 0  ; 
+        }else{
+            suffix[n-1] = 1 ;
+        }
+        
+         
+       for(int i = s.length() - 2 ; i >= 0  ; i--){
+           char ch = s.charAt(i);
+            if(s.charAt(i) == '1'){
+                suffix[i] = suffix[i+1];
+                ans += suffix[i];
+            }else{
+                suffix[i] =  suffix[i+1]+1 ; 
+            }
+        }
+        return ans ; 
+    }
+}
+/**
+ int left = 0;
+        int right = 0;
+        char[] arr = s.toCharArray();
+        int count = 0;
+        while (right < s.length() && left < s.length()) {
+            if (arr[right] == '1') {
+                right++;
+            } else {
+                if (arr[left] == '0') {
+                    while (arr[left] != '1' && left < right) {
+                        left++;
+                    }
+                }
+                if(arr[left] != arr[right] && left < right){
+                    char temp = arr[right];
+                    arr[right] = arr[left];
+                    arr[left] = temp;
+                    count++;
+                    left++;
+                    right++;
+                }else{
+                    right++;
+                }
+                     
+            }
+        }
+        return count; */
