c345fb729d6267c3e25fbf01be8d8df6ffe0f250
parshant4597 <149263314+parshant4597@users.noreply.github.com>
2026-06-23 01:31:14 +0530

    Time: 865 ms (6.63%), Space: 154.8 MB (13.26%) - LeetHub

---
+++ 3346-maximum-frequency-of-an-element-after-performing-operations-i/3346-maximum-frequency-of-an-element-after-performing-operations-i.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int maxFrequency(int[] nums, int k, int numOperations) {
+        Map<Integer, Integer> cnt = new HashMap<>();
+        TreeMap<Integer, Integer> lineSweep = new TreeMap<>();
+         
+        for (int x : nums) {
+            cnt.put(x, cnt.getOrDefault(x, 0) + 1);
+             
+            lineSweep.put(x - k, lineSweep.getOrDefault(x - k, 0) + 1);
+            lineSweep.put(x + k + 1, lineSweep.getOrDefault(x + k + 1, 0) - 1); 
+            lineSweep.put(x, lineSweep.getOrDefault(x, 0) + 0);
+        }
+        
+        int ans = 0;
+        int currentOverlapping = 0;
+         
+        for (Map.Entry<Integer, Integer> entry : lineSweep.entrySet()) {
+            int point = entry.getKey();
+            currentOverlapping += entry.getValue();
+            
+            int exactCount = cnt.getOrDefault(point, 0);
+            
+            // Apply the formula
+            int achievable = exactCount + Math.min(numOperations, currentOverlapping - exactCount);
+            ans = Math.max(ans, achievable);
+        }
+        
+        return ans;
+    }
+}
