ba6f8eced30fbf14912a789e5186038d5e001bcb
parshant4597 <149263314+parshant4597@users.noreply.github.com>
2026-06-22 15:35:18 +0530

    Time: 28 ms (93.23%), Space: 108.2 MB (99.48%) - LeetHub

--- 1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.java
+++ 1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.java
@@ -2,47 +2,33 @@ class Solution {
     public int maxSumRangeQuery(int[] nums, int[][] req) {
         int n = nums.length;
 
-        List<int[]> ls = new ArrayList<>();
+        
+        int[] ls = new int[nums.length+1];
 
         for (int i = 0; i < req.length; i++) {
             int l = req[i][0];
             int r = req[i][1];
 
-            ls.add(new int[]{l, 1});
-            ls.add(new int[]{r + 1, -1});
+            ls[l]++;
+            ls[r+1]--;
+        }
+ 
+
+        int[] actualFreq = new int[n];
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += ls[i];
+            actualFreq[i] = sum;
         }
 
-        Collections.sort(ls, (a, b) -> a[0] - b[0]);
-
-        ArrayList<Integer> freq = new ArrayList<>();
-
-        int active = 0;
-        int prev = 0;
-
-        for (int[] event : ls) {
-            int pos = Math.min(event[0], n);
-
-            while (prev < pos) {
-                freq.add(active);
-                prev++;
-            }
-
-            active += event[1];
-        }
-
-        while (prev < n) {
-            freq.add(active);
-            prev++;
-        }
-
-        Collections.sort(freq);
+        Arrays.sort(actualFreq);
         Arrays.sort(nums);
 
         long ans = 0;
         long mod = 1_000_000_007L;
 
         for (int i = 0; i < n; i++) {
-            ans = (ans + (long) freq.get(i) * nums[i]) % mod;
+            ans = (ans + (long) actualFreq[i] * nums[i]) % mod;
         }
 
         return (int) ans;
