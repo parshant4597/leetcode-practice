43b742e9149a354b2b487c4b64ea199e06c11c67
parshant4597 <149263314+parshant4597@users.noreply.github.com>
2026-06-23 01:51:54 +0530

    Time: 469 ms (17.68%), Space: 192.9 MB (5.52%) - LeetHub

--- 3346-maximum-frequency-of-an-element-after-performing-operations-i/3346-maximum-frequency-of-an-element-after-performing-operations-i.java
+++ 3346-maximum-frequency-of-an-element-after-performing-operations-i/3346-maximum-frequency-of-an-element-after-performing-operations-i.java
@@ -1,32 +1,39 @@
-import java.util.*;
-
 class Solution {
-    public int maxFrequency(int[] nums, int k, int numOperations) {
-        Map<Integer, Integer> cnt = new HashMap<>();
-        TreeMap<Integer, Integer> lineSweep = new TreeMap<>();
-         
-        for (int x : nums) {
-            cnt.put(x, cnt.getOrDefault(x, 0) + 1);
-             
-            lineSweep.put(x - k, lineSweep.getOrDefault(x - k, 0) + 1);
-            lineSweep.put(x + k + 1, lineSweep.getOrDefault(x + k + 1, 0) - 1); 
-            lineSweep.put(x, lineSweep.getOrDefault(x, 0) + 0);
+    public int maxFrequency(int[] nums, int k, int numop) {
+        Map<Integer, Integer> map  = new HashMap<>();
+        for(int i = 0 ; i < nums.length ; i++){
+            map.put(nums[i] , map.getOrDefault(nums[i], 0)+1);
         }
-        
-        int ans = 0;
-        int currentOverlapping = 0;
-         
-        for (Map.Entry<Integer, Integer> entry : lineSweep.entrySet()) {
-            int point = entry.getKey();
-            currentOverlapping += entry.getValue();
-            
-            int exactCount = cnt.getOrDefault(point, 0);
-            
-            // Apply the formula
-            int achievable = exactCount + Math.min(numOperations, currentOverlapping - exactCount);
-            ans = Math.max(ans, achievable);
+        ArrayList<int[]> arr = new ArrayList<>();
+        for(int i = 0 ; i < nums.length ; i++){
+            int st = nums[i] - k ;
+            int end = nums[i]+ k ; 
+            arr.add(new int[]{st  , 1});
+            arr.add(new int[]{end +1, -1});
+            arr.add(new int[]{nums[i] , 0});
         }
+        Collections.sort(arr , (a , b) ->{
+            if(a[0]== b[0])return a[1] - b[1];
+            return a[0] - b[0];
+        });
+        int maxfreq =  0 ; 
+        int count = 0;
+         
+
+        for(int i = 0 ; i < arr.size() ; i++){
+            int point = arr.get(i)[0];
+            count += arr.get(i)[1];
+            int dup = map.getOrDefault(point , 0 );
+            int aci = dup + Math.min(numop , count - dup);
+            maxfreq = Math.max(maxfreq , aci);
+        }
+        return maxfreq ; 
         
-        return ans;
+        
+         
+         
+
+ 
+        
     }
 }
