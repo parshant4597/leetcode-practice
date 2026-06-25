648869bf904322d42bf43ed6250d0c6de8c493b9
parshant4597 <149263314+parshant4597@users.noreply.github.com>
2026-06-25 12:51:40 +0530

    Time: 4 ms (75.53%), Space: 49.9 MB (29.19%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<String> list ;
+    int prev = -1;
+    public void solve(TreeNode root , String res){
+        if(root.left == null && root.right == null){ 
+            res += root.val;
+            if(!list.contains(res)){
+                list.add(res);
+            }
+            return ; 
+        }  
+        res += root.val+ "->";     
+        if(root.left != null){     
+            solve(root.left , res);
+        }
+        if(root.right != null){ 
+            solve(root.right , res);
+
+        }
+          
+    }
+    public List<String> binaryTreePaths(TreeNode root) {
+        list = new ArrayList<>();
+        if(root == null){
+            return list ; 
+        }
+        solve(root , "");
+        return list ;
+    }
+}
