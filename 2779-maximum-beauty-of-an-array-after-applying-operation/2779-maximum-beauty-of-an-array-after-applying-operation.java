275fdddd456ac900d1a7bdae62eab1a607e69c3d
parshant4597 <149263314+parshant4597@users.noreply.github.com>
2026-06-26 01:27:19 +0530

    Time: 397 ms (5.22%), Space: 164.2 MB (5.22%) - LeetHub

---
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maximumBeauty(int[] nums, int k) {
+        List<int[]> arr = new ArrayList<>();
+        for(int i= 0 ; i < nums.length ; i++){
+            int st = nums[i]- k ;
+            int end = nums[i]+k ;
+            arr.add(new int[]{st , 1});
+            arr.add(new int[]{end+1 , -1});
+            arr.add(new int[]{nums[i] , 0 });
+        }
+        Collections.sort(arr , (a, b) ->{
+            if(a[0] == b[0])return a[1] - b[1];
+            return a[0] - b[0];
+        });
+        int max = 0 ;
+        int sum = 0 ; 
+        for(int i = 0 ; i < arr.size() ; i++){
+            sum += arr.get(i)[1];
+            max  = Math.max(sum , max);
+
+        }
+        return max; 
+
+        
+    }
+}
