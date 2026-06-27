a3af0efeb570afd94bb968df51b6dc45d41d98c4
parshant4597 <149263314+parshant4597@users.noreply.github.com>
2026-06-27 18:08:51 +0530

    Time: 1 ms (100%), Space: 47.2 MB (78.16%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.java
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.java
@@ -1,28 +1,58 @@
+// class Solution {
+//     public int[] resultsArray(int[] nums, int k) {
+//         int n = nums.length ; 
+//         int[] res =  new int[n- k +1];
+        
+//         int left = 0 ; 
+//         int right = k -1 ; 
+//         int i = 0 ;
+//         while(right < n ){
+//             int prev = nums[left];
+//             boolean flag = true ; 
+//             for(int d = left+1 ; d <= right ; d++){
+//                 if(prev != nums[d]-1){
+//                     flag = false;
+//                 }
+//                 prev = nums[d];
+//             }
+//             if(flag){
+//                 res[i++] = nums[right];
+//             }else{
+//                 res[i++] = -1;
+//             }
+//             right++;
+//             left++;
+//         }     
+//         return res ;   
+//     }
+// }
 class Solution {
     public int[] resultsArray(int[] nums, int k) {
-        int n = nums.length ; 
-        int[] res =  new int[n- k +1];
+        int n = nums.length;
+        int[] results = new int[n - k + 1];
         
-        int left = 0 ; 
-        int right = k -1 ; 
-        int i = 0 ;
-        while(right < n ){
-            int prev = nums[left];
-            boolean flag = true ; 
-            for(int d = left+1 ; d <= right ; d++){
-                if(prev != nums[d]-1){
-                    flag = false;
+        // A counter to track the length of the current consecutive sequence
+        int consecutiveCount = 1;
+        
+        for (int i = 0; i < n; i++) {
+            // If not the first element, check if the current is consecutive to the previous
+            if (i > 0 && nums[i] == nums[i - 1] + 1) {
+                consecutiveCount++;
+            } else if (i > 0) {
+                // Reset to 1 if the sequence breaks
+                consecutiveCount = 1;
+            }
+            
+            // Once we have processed at least k elements, start recording results
+            if (i >= k - 1) {
+                if (consecutiveCount >= k) {
+                    results[i - k + 1] = nums[i];
+                } else {
+                    results[i - k + 1] = -1;
                 }
-                prev = nums[d];
             }
-            if(flag){
-                res[i++] = nums[right];
-            }else{
-                res[i++] = -1;
-            }
-            right++;
-            left++;
-        }     
-        return res ;   
+        }
+        
+        return results;
     }
 }
