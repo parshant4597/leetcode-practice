b435896ace9926bc7fac5b9b177a7b1ac3d7fe90
parshant4597 <149263314+parshant4597@users.noreply.github.com>
2026-06-29 00:52:45 +0530

    Time: 32 ms (6.67%), Space: 81.7 MB (5%) - LeetHub

---
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        Arrays.sort(arr);
+        if(arr[0] != 1){
+            arr[0] = 1; 
+        }
+        int prev = arr[0];
+        for(int i= 1  ; i < arr.length ; i++ ){
+            int cur = arr[i];
+            if (arr[i] > prev + 1) {
+                arr[i] = prev + 1;
+            }
+            prev = arr[i]; 
+        }
+        System.out.println(Arrays.toString(arr));
+        return arr[arr.length - 1];
+    }
+}
