7170bb84cbba420b0b1c45b0d2111288575ec0dc
parshant4597 <149263314+parshant4597@users.noreply.github.com>
2026-06-29 12:30:15 +0530

    Time: 0 ms (100%), Space: 44.9 MB (78.3%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,32 +1,51 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  *     int val;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode() {}
+//  *     TreeNode(int val) { this.val = val; }
+//  *     TreeNode(int val, TreeNode left, TreeNode right) {
+//  *         this.val = val;
+//  *         this.left = left;
+//  *         this.right = right;
+//  *     }
+//  * }
+//  */
+// class Solution {
+//     ArrayList<Integer> arr ; 
+//     public void solve(TreeNode root){
+//          if(root == null)return ;
+//          solve(root.left);
+//          arr.add(root.val);
+//          solve(root.right);
+//     }
+//     public boolean isValidBST(TreeNode root) {
+//         arr = new ArrayList<>();
+//          solve(root);
+//          for(int i = 1 ; i < arr.size()  ; i++){
+//             if(arr.get(i) <= arr.get(i-1))return false; 
+//          }
+//          return true ; 
+//     }
+// }
 class Solution {
-    ArrayList<Integer> arr ; 
-    public void solve(TreeNode root){
-         if(root == null)return ;
-         solve(root.left);
-         arr.add(root.val);
-         solve(root.right);
-    }
     public boolean isValidBST(TreeNode root) {
-        arr = new ArrayList<>();
-         solve(root);
-         for(int i = 1 ; i < arr.size()  ; i++){
-            if(arr.get(i) <= arr.get(i-1))return false; 
-         }
-         return true ; 
+        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
+
+    boolean helper(TreeNode root, long val1, long val2){
+
+        if(root == null){
+            return true;
+        }
+        if(root.val <= val1 || root.val >= val2){
+            return false;
+        }
+        
+        return helper(root.left, val1, root.val) && helper(root.right, root.val, val2);
+    }
+
+    
 }
