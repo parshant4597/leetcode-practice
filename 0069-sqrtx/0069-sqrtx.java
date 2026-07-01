9b6fcf5aa79ccd60f4dc864fe785ae7385ad799a
parshant4597 <149263314+parshant4597@users.noreply.github.com>
2026-07-02 01:16:40 +0530

    Time: 2 ms (11.9%), Space: 42.4 MB (89.5%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int mySqrt(int x) {
+        return (int)findSquareRoot(x , 100);
+    }
+ 
+    public   double findSquareRoot(double number, int iterations) {
+      
+        if (number < 0) {
+            throw new IllegalArgumentException("Cannot calculate the square root of a negative number.");
+        }
+        if (number == 0) {
+            return 0;
+        }
+
+        // Start with an initial guess (half of the target number)
+        double guess = number / 2.0;
+
+        // Run the approximation loop
+        for (int i = 0; i < iterations; i++) {
+            guess = (guess + (number / guess)) / 2.0;
+        }
+
+        return guess;
+    }
+
+    
+}
