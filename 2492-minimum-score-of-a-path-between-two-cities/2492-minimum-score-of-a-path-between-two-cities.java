bf6b085cb11ffa797ae35a4cddba6dd0403c67dd
parshant4597 <149263314+parshant4597@users.noreply.github.com>
2026-07-05 01:51:28 +0530

    Time: 233 ms (5.04%), Space: 134 MB (90.35%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
@@ -1,46 +1,45 @@
-class DSU {
-    int[] parent;
-
-    public DSU(int n) {
-        parent = new int[n + 1];
-        for (int i = 1; i <= n; i++)
-            parent[i] = i;
+class DSU{
+    int[] parent ; 
+    public DSU(int n ){
+        parent = new int[n+1]; 
+        for(int i = 0 ; i < n+1 ; i++){
+            parent[i] = i ; 
+        }
     }
-
-    public int findparent(int u) {
-        if (parent[u] == u)
-            return u;
+    public int findparent(int u){
+        if(u == parent[u] )return u ; 
         return parent[u] = findparent(parent[u]);
     }
-
-    public void union(int u, int v) {
+    public void union(int u  , int v){
         int up = findparent(u);
-        int vp = findparent(v);
-
-        if (up == vp)
-            return;
-
-        parent[vp] = up;
+        int vp  = findparent(v);
+        if(up == vp)return  ; 
+        
+        parent[vp] = up ;
+      
     }
 }
-
 class Solution {
+
     public int minScore(int n, int[][] roads) {
-
-        DSU dsu = new DSU(n);
-
-        for (int[] road : roads) {
-            dsu.union(road[0], road[1]);
+        // dsu bnao usme ek par
+         
+        DSU dsu  = new DSU(n);
+        for(int[] road : roads){
+            int u = road[0];
+            int v = road[1];
+             
+            int cost = road[2];
+             
+            dsu.union(u , v);
         }
-
-        int ans = Integer.MAX_VALUE;
-
-        for (int[] road : roads) {
-            if (dsu.findparent(road[0]) == dsu.findparent(1)) {
-                ans = Math.min(ans, road[2]);
-            }
-        }
-
-        return ans;
+        int min = Integer.MAX_VALUE ; 
+        for(int[] road : roads){
+            int u = road[0];
+            int v = road[1];
+            int cost = road[2];
+            if(dsu.findparent(u) == dsu.findparent(1)) min = Math.min(min , cost);
+        }  
+        return min;
     }
 }
