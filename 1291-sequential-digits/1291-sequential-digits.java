c2c1ec29fd63eb6d8281e140592c6f4edd2d2395
parshant4597 <149263314+parshant4597@users.noreply.github.com>
2026-07-13 13:52:23 +0530

    Time: 0 ms (100%), Space: 42.7 MB (6.32%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.java
+++ 1291-sequential-digits/1291-sequential-digits.java
@@ -4,16 +4,11 @@ class Solution {
         for(int i = 1 ; i <= 9 ; i++){
             int num = 0; 
             int count = i; 
-            while(  count <= 9  ){
-                num = num*10 + count; 
+            while(num <= high && count <= 9  ){
+                num= num*10 + count; 
                 if (num >= low && num <= high) {
                     ans.add(num);
                 }
-                
-                if (num > high) {
-                    break;  
-                }
-                 
                 count++;
             }
             
