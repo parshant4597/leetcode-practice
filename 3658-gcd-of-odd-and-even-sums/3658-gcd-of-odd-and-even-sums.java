8e62c42c4af8c7b71dc5dc54ba37bfe4f3d7c127
parshant4597 <149263314+parshant4597@users.noreply.github.com>
2026-07-15 15:21:50 +0530

    Time: 0 ms (100%), Space: 42.5 MB (57.16%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,3 +1,4 @@
+ 
 class Solution {
     public static int gcd(int a, int b) {
         if (b == 0) {
@@ -6,13 +7,8 @@ class Solution {
         return gcd(b, a % b);
     }
     public int gcdOfOddEvenSums(int n) {
-        int sumodd =  0 ; 
-        int sumeven =0 ;
-        for(int i = 1 ; i <= 2*n ; i++){
-            if(i % 2== 0)sumeven+= i;
-            else sumodd += i;
-        }
-        int ans = gcd(sumodd , sumeven);
-        return ans; 
+         
+        return n;
+ 
     }
 }
