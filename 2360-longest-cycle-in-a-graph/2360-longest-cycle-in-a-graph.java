b8a72c726145fb76e31924432b0924258924c909
parshant4597 <149263314+parshant4597@users.noreply.github.com>
2026-07-24 22:23:21 +0530

    Time: 65 ms (35.51%), Space: 154.3 MB (17.82%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
@@ -1,29 +1,35 @@
 class Solution {
-    int answer = -1;
-
-    public void dfs(int node, int[] edges, Map<Integer, Integer> dist, boolean[] visit) {
-        visit[node] = true;
-        int neighbor = edges[node];
-
-        if (neighbor != -1 && !visit[neighbor]) {
-            dist.put(neighbor, dist.get(node) + 1);
-            dfs(neighbor, edges, dist, visit);
-        } else if (neighbor != -1 && dist.containsKey(neighbor)) {
-            answer = Math.max(answer, dist.get(node) - dist.get(neighbor) + 1);
+    int ans = -1;
+    public void dfs(boolean[] vis ,int node ,  int[] edges ,HashMap<Integer, Integer> map ){
+        vis[node] = true ;
+        int nei = edges[node];
+        if(nei != -1 && !vis[nei]){  
+            map.put(nei , map.get(node)+1);
+            dfs(vis , nei , edges , map);
+        }else if(nei != -1 && map.containsKey(nei)){
+            ans = Math.max(ans, map.get(node) - map.get(nei) + 1);
         }
     }
-
     public int longestCycle(int[] edges) {
-        int n = edges.length;
-        boolean[] visit = new boolean[n];
-
-        for (int i = 0; i < n; i++) {
-            if (!visit[i]) {
-                Map<Integer, Integer> dist = new HashMap<>();
-                dist.put(i, 1);
-                dfs(i, edges, dist, visit);
+        int n = edges.length ;
+        int[] in = new int[n]; 
+        for(int i = 0 ; i < n ; i++){
+            int node = edges[i];
+            if(node == -1)continue;
+            in[node]++;
+        }
+        boolean[] vis = new boolean[n];
+        for(int i = 0 ;i < n ; i++){
+            vis[i] = false ; 
+        }
+        for(int i = 0 ; i < edges.length ; i++){
+            if(!vis[i] && in[i] > 0 && edges[i] != -1 ){
+                HashMap<Integer, Integer> map= new HashMap<>();
+                map.put(i , 1);
+                dfs(vis  ,i ,  edges , map);
             }
         }
-        return answer;
+        return ans ; 
+
     }
 }
