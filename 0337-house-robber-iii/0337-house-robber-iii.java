ad8ee4919f4037ef1b6fd5764126f6e65939b42a
parshant4597 <149263314+parshant4597@users.noreply.github.com>
2026-07-28 12:30:35 +0530

    Time: 0 ms (100%), Space: 46.5 MB (58.78%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.java
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -1,42 +1,63 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  *     int val;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode() {}
+//  *     TreeNode(int val) { this.val = val; }
+//  *     TreeNode(int val, TreeNode left, TreeNode right) {
+//  *         this.val = val;
+//  *         this.left = left;
+//  *         this.right = right;
+//  *     }
+//  * }
+//  */
+// class Solution {
+    
+//     public int rob(TreeNode root) {
+
+//         return solve(root, new HashMap<>());
+//     }
+
+//     private int solve(TreeNode root, Map<TreeNode, Integer> map) {
+//         if (root == null)
+//             return 0;
+//         if (map.containsKey(root))
+//             return map.get(root);
+
+//         int val = 0;
+
+//         if (root.left != null) {
+//             val += solve(root.left.left, map) + solve(root.left.right, map);
+//         }
+
+//         if (root.right != null) {
+//             val += solve(root.right.left, map) + solve(root.right.right, map);
+//         }
+
+//         val = Math.max(val + root.val, solve(root.left, map) + solve(root.right, map));
+//         map.put(root, val);
+
+//         return val;
+//     }
+// }
+
 class Solution {
     public int rob(TreeNode root) {
-        return robSub(root, new HashMap<>());
+        int[] result = robTree(root);
+        return Math.max(result[0], result[1]);
     }
 
-    private int robSub(TreeNode root, Map<TreeNode, Integer> map) {
-        if (root == null)
-            return 0;
-        if (map.containsKey(root))
-            return map.get(root);
-
-        int val = 0;
-
-        if (root.left != null) {
-            val += robSub(root.left.left, map) + robSub(root.left.right, map);
+    private int[] robTree(TreeNode root) {
+        if (root == null) {
+            return new int[]{0, 0}; 
         }
-
-        if (root.right != null) {
-            val += robSub(root.right.left, map) + robSub(root.right.right, map);
-        }
-
-        val = Math.max(val + root.val, robSub(root.left, map) + robSub(root.right, map));
-        map.put(root, val);
-
-        return val;
+        int[] left = robTree(root.left);
+        int[] right = robTree(root.right);
+        int[] dp = new int[2]; 
+        dp[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]); 
+        dp[1] = root.val + left[0] + right[0];
+        return dp;
     }
 }
