26bcee9d876bdc568b4096dfd6cc306044d831a8
parshant4597 <149263314+parshant4597@users.noreply.github.com>
2026-09-18 06:46:22 +0530

    Time: 291 ms (69.42%), Space: 0B (100%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,10 @@
+-- # Write your MySQL query statement below
+--  with table1 as (select dense_rank() over(order by salary desc) as rnk , salary   from Employee)
+
+--  select max(salary) as SecondHighestSalary    from table1 where rnk = 2 ;
+-- method 1 
+with table1 as (
+    select * , dense_rank() over(order by salary desc) as rnk from Employee 
+)
+select case when count(salary) >= 1 then salary
+else  null end as SecondHighestSalary   from table1 where rnk = 2 limit 1 ;  
