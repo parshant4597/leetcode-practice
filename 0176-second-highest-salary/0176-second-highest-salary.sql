cb304dc7275de7aee08cede8d5a8f4e5cd9cc4d9
parshant4597 <149263314+parshant4597@users.noreply.github.com>
2026-09-18 06:48:55 +0530

    Time: 335 ms (28.06%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -3,8 +3,13 @@
 
 --  select max(salary) as SecondHighestSalary    from table1 where rnk = 2 ;
 -- method 1 
-with table1 as (
-    select * , dense_rank() over(order by salary desc) as rnk from Employee 
+-- with table1 as (
+--     select * , dense_rank() over(order by salary desc) as rnk from Employee 
+-- )
+-- select case when count(salary) >= 1 then salary
+-- else  null end as SecondHighestSalary   from table1 where rnk = 2 limit 1 ;  
+select max(m.salary) as SecondHighestSalary 
+from Employee m 
+where m.salary < (
+    select max(e.salary) from Employee e 
 )
-select case when count(salary) >= 1 then salary
-else  null end as SecondHighestSalary   from table1 where rnk = 2 limit 1 ;  
